06f5d40d277ef1b3f32393d50a197aa914dff9f0
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-08-19 16:47:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var noseX = 0, noseY = 0, diff = 0, leftX = 0, rightX = 0;
 function setup() {
     canvas = createCanvas(550, 510);
     canvas.position(800,160);
@@ -10,6 +11,9 @@ function setup() {
 
 function draw() {
     background('#2d2d2d');
+    fill('#FFFFFF');
+    stroke('#000000');
+    square(noseX, noseY, 100);
 }
 
 function modelLoaded() {
@@ -19,5 +23,10 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(result);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftX = Math.floor(results[0].pose.leftWrist.x);
+        rightX = Math.floor(results[0].pose.rightWrist.x);
+        diff = Math.abs(leftX - rightX);
     }
 }
